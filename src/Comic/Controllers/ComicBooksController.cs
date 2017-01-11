4a4e5500cae0daaf9d597a4459b10133150b7f1e
Brenden Seidel <brenseid@gmail.com>
2017-01-11 15:29:03 -0500

    Added detail action method

--- src/Comic/Controllers/ComicBooksController.cs
+++ src/Comic/Controllers/ComicBooksController.cs
@@ -7,5 +7,9 @@ namespace Comic.Controllers
 {
     public class ComicBooksController : System.Web.Mvc.Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
