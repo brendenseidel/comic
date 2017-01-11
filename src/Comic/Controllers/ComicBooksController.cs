de7205c43767232d97aba190a1727b5818d36dfb
Brenden Seidel <brenseid@gmail.com>
2017-01-11 15:23:05 -0500

    Added Comic Books Controller

---
+++ src/Comic/Controllers/ComicBooksController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comic.Controllers
+{
+    public class ComicBooksController : System.Web.Mvc.Controller
+    {
+    }
+}
